00d5009c6dd4260e7d8c29d0f48efd865cf93c9a
Anaswara M <anaswaramanoj007@gmail.com>
2020-12-20 13:49:52 +0530

    input from textarea

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 
 function clickHandler(){
     console.log("clicked!");
+    console.log(txtInput.value);
 }
 
 btnTranslate.addEventListener("click", clickHandler)
