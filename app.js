da03d18c2e2b29b5fe794098b9f29dbe8fe7d1bd
Anaswara M <anaswaramanoj007@gmail.com>
2020-12-20 13:57:40 +0530

    output

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 function clickHandler(){
     console.log("clicked!");
     console.log(txtInput.value);
+    outputDiv.innerText=txtInput.value;
 }
 
 btnTranslate.addEventListener("click", clickHandler)
